827bcb9430ebcf1eef651957b676c96c3fc4786a
FIRAS <97522852+fifolio@users.noreply.github.com>
2024-01-30 13:16:23 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import Todo from "./Todo";
+
+export default function App() {
+  useEffect(() => {  
+    document.title = "Lemme App";
+  }, []);
+
+
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  )
+}
---
+++ src/Todo.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default function Todo() {
+  return (
+    <div className="main-container">
+      <h1>Todo List</h1>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+      </ul>
+      <input type="text" placeholder="Add todo item" />
+      <button type="submit">Submit</button>
+    </div>
+  )
+}

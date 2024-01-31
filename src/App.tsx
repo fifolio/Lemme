47b762b0b69f3e8ded31fb2e63b2f570cc6f600c
FIRAS <97522852+fifolio@users.noreply.github.com>
2024-01-31 13:48:22 +0000

    beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
-import Todo from "./Todo";
+import { Todo } from "./Todo";
 
 export default function App() {
-  useEffect(() => {  
+  useEffect(() => {
     document.title = "Lemme App";
   }, []);
 
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,16 +1,48 @@
+import { useState } from "react";
 
+interface item {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
+export const Todo: React.FC = () => {
 
-export default function Todo() {
+  const [input, setInput] = useState<string>("")
+
+  const [todos, setTodos] = useState<item[]>([
+    { id: 1, text: 'Hello TS', completed: false },
+    { id: 2, text: 'Bye JS', completed: false }
+  ])
+
+  const handleToggle = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id ? {...todo, completed: !todo.completed} : todo;
+      })
+    )
+  }
+
+  const handleAdd = () => {
+    const newTodo: item = {
+      id: Date.now(),
+      text: input,
+      completed: false
+    }
+
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <div className="main-container">
       <h1>Todo List</h1>
       <ul>
-        <li>item 1</li>
-        <li>item 2</li>
+        {todos.map((todo) => (
+          <li key={todo.id} onClick={() => handleToggle(todo.id)} style={{textDecoration: todo.completed ? "line-through" : "none"}}>{todo.text}</li>
+        ))}
       </ul>
-      <input type="text" placeholder="Add todo item" />
-      <button type="submit">Submit</button>
+      <input type="text" placeholder="Add todo item" onChange={(e) => setInput(e.currentTarget.value)}/>
+      <button type="submit" onClick={handleAdd}>Add</button>
     </div>
   )
 }
